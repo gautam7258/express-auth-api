4464ea90d33eac4a9454eea0e10ab3071d9aac69
gk-weblab <127711023+gk-weblab@users.noreply.github.com>
2023-08-06 11:40:10 +0530

    Update authController.js
    30s refrewsh

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,7 +34,7 @@ const handleLogin = async (req, res) => {
 		const refreshToken = jwt.sign(
 			{ username: user },
 			process.env.REFRESH_TOKEN_SECRET,
-			{ expiresIn: "15s" }
+			{ expiresIn: "30s" }
 		);
 		//update the refreshToken in database
 	
